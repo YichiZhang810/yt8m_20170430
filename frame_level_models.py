d5497aba59ce588ef53a01db913222ea5745095c
YichiZhang810 <yichizhangmax@utexas.edu>
2017-04-30 16:36:43 -0500

    Add files via upload

--- frame_level_models.py
+++ frame_level_models.py
@@ -227,10 +227,10 @@ class LstmModel(models.BaseModel):
 
     loss = 0.0
 
+    cell = rnn_cell_modern.HighwayRNNCell(512, num_highway_layers = 3)
     outputs, state = tf.nn.dynamic_rnn(stacked_lstm, model_input,
                                        sequence_length=num_frames,
                                        dtype=tf.float32)
-
     aggregated_model = getattr(video_level_models,
                                FLAGS.video_level_classifier_model)
 
