fbceb21eb2a1c7ef1bae9686a5322ae7f3195e9d
YichiZhang810 <yichizhangmax@utexas.edu>
2017-04-30 23:49:25 -0500

    Add files via upload

--- frame_level_models.py
+++ frame_level_models.py
@@ -275,7 +275,7 @@ class LstmModel(models.BaseModel):
     print('-------------')
 
 
-    outputs, state = tf.nn.dynamic_rnn(cell, model_input,
+    outputs, state = tf.nn.dynamic_rnn(stacked_lstm, model_input,
                                        sequence_length=num_frames,
                                        dtype=tf.float32)
 
